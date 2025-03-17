7436533428ff274194136bac445f8e8de01c42a9
Bar Perach <3817158+barp@users.noreply.github.com>
2025-03-17 22:23:42 +0900

    add open float for diagnostic

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -8,5 +8,7 @@ map("i", "<C-l>", "<cmd>lua vim.fn.feedkeys(vim.fn['copilot#Accept'](), '')<CR>"
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
+map("n", "<C-k>", "<cmd>lua vim.diagnostic.open_float()<CR>", { desc = "Open diagnostic float" })
+
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
